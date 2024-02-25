e7f7cd97a27ec9ecba60c21de67386002bdc51ea
Harshal Laheri <harshal@harshallaheri.me>
2024-02-25 11:03:17 -0500

    backend api

--- src/App.js
+++ src/App.js
@@ -4,54 +4,55 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
-const location = {
-  address: "Salmon",
-  lat: 37.42216,
-  lng: -122.08427,
-};
+// const location = {
+//   address: "Salmon",
+//   lat: 37.42216,
+//   lng: -122.08427,
+// };
 function App() {
-  // Call Login API and save the token in the context
+    // Call Login API and save the token in the context
 
-  const [token, setToken] = useState("");
+    const [token, setToken] = useState("");
 
-  const [fishLocation, setFishLocation] = useState([]);
+    const [fishLocation, setFishLocation] = useState([]);
 
-  useEffect(() => {
-    fetch(`/login/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: "admin",
-        password: "admin@1234",
-      }),
-    })
-      .then((res) => res.json())
-      .then((response) => {
-        setToken(response.token);
-      });
-  }, []);
+    useEffect(() => {
+        fetch("login/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: "admin",
+                password: "admin@1234",
+            }),
+        })
+            .then((res) => res.json())
+            .then((response) => {
+                setToken(response.token);
+            });
+    }, []);
 
-  useEffect(() => {
-    fetch(`/fish-location?country=Canada`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((response) => {
-        setFishLocation(response.data);
-      });
-  }, [token]);
+    useEffect(() => {
+        fetch("fish-location?country=Canada", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((response) => {
+                setFishLocation(response);
+                console.log(response);
+            });
+    }, [token]);
 
-  return (
-    <div className="App">
-      <MapSection location={location} zoomLevel={17} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <MapSection location={fishLocation} zoomLevel={17} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -5,32 +5,36 @@ import "./map.css";
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker";
 
-const Map = ({ location, zoomLevel }) => (
-  <div className="map">
-    <h2 className="map-h2">GSC-24 FE</h2>
-    <div className="google-map">
-    {/*  Loop to the array of markers and point the location */}
-
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyA2C03GDZLtdQ5V1x-T95R3kd5kyJrwpz4" }} // GCP API Key
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-      >
-        <LocationPin
+const Map = ({ fishLocation, zoomLevel }) => (
+    console.log(fishLocation),
+    (
+        <div className="map">
+            <h2 className="map-h2">GSC-24 FE</h2>
+            <div className="google-map">
+                {/*  Loop to the array of markers and point the location */}
+                <GoogleMapReact
+                    bootstrapURLKeys={{
+                        key: "AIzaSyA2C03GDZLtdQ5V1x-T95R3kd5kyJrwpz4",
+                    }} // GCP API Key
+                    defaultCenter={{ lat: 37.42216, lng: -122.08427 }}
+                    defaultZoom={zoomLevel}
+                >
+                    {/* <LocationPin
           lat={location.lat}
           lng={location.lng}
           text={location.address}
-        />
-      </GoogleMapReact>
-    </div>
-  </div>
+        /> */}
+                </GoogleMapReact>
+            </div>
+        </div>
+    )
 );
 
 const LocationPin = ({ text }) => (
-  <div className="pin">
-    <Icon icon={locationIcon} className="pin-icon" />
-    <p className="pin-text">{text}</p>
-  </div>
+    <div className="pin">
+        <Icon icon={locationIcon} className="pin-icon" />
+        <p className="pin-text">{text}</p>
+    </div>
 );
 
 export default Map;
